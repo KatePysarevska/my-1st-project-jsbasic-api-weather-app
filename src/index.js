a9ab9d7c76d771ea6b834e0d9bcd31c4242cd481
Kate Pysarevska <katepr888@gmail.com>
2023-12-28 20:59:40 +0000

    added new html, css and js files for forecast data

--- src/index.js
+++ src/index.js
@@ -16,6 +16,8 @@ function refreshWeather(response) {
   humidity.innerHTML = `${response.data.temperature.humidity} %`;
   wind.innerHTML = `${response.data.wind.speed} km/h`;
   iconElement.innerHTML = ` <img src="${response.data.condition.icon_url}" class="weather-app-icon" />`;
+
+  getForecast(response.data.city);
 }
 
 function formatDate(date) {
@@ -49,6 +51,46 @@ function handleSearchSubmit(event) {
   let searchInputElement = document.querySelector("#search-form-input");
 
   searchCity(searchInputElement.value); // Fixed the variable name
+}
+
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
+  return days[date.getDay()];
+}
+
+function getForecast(city) {
+  let apiKey = "8a3517e0c164a33t7a9df4a7b0b7dc8o";
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
+
+function displayForecast(response) {
+  console.log(response.data);
+  let forecastHtml = "";
+
+  response.data.daily.forEach(function (day, index) {
+    if (index < 5) {
+      forecastHtml =
+        forecastHtml +
+        `
+  <div clas="weather-forecast-day">
+  <div class="weather-forecast-date">${formatDay(day.time)}</div>
+   <img class="icon" src="${day.condition.icon_url}" /> 
+  <div  class="weather-forecast-temperature">
+  <span class="weather-forecast-temperature-max">${Math.round(
+    day.temperature.maximum
+  )}</span> 
+  <span class="weather-forecast-temperature-min">${Math.round(
+    day.temperature.minimum
+  )}</span>
+  </div>  
+     
+  </div>`;
+    }
+  });
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = forecastHtml;
 }
 
 let searchFormElement = document.querySelector("#search-form");
